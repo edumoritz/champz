d668ea29056aa768250889a929c984ad1a9f359f
edumoritz <eduardo.moritz@hotmail.com>
2022-06-26 23:10:48 -0300

    feature:create project frontend

---
+++ README.md
@@ -0,0 +1,2 @@
+# Champz Project
+
---
+++ champz-frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {SidebarModule} from 'primeng/sidebar';
+import {ButtonModule} from 'primeng/button';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SidebarModule,
+    ButtonModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
